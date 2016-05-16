d9bf942d1cdd33d0a0fbd14419dbbee66fb038a9
timothyjamesjensen <timothyjamesjensen@gmail.com>
2016-05-15 17:39:44 -0700

    Question 3 part 2 done

--- StevieFun/byref.s
+++ StevieFun/byref.s
@@ -77,3 +77,23 @@ Xincrement:
 	popq	%rbp
 	ret
 
+# OUTPUT JUSTIFICATION
+#
+# This assembly file produces the following output:
+#
+# output: 50
+# output: 26
+#
+# This output is without a doubt the expected output.
+# The values 25 and 23 are passed by reference into
+# the byref() method. These values are then each passed
+# by reference into redirect and increment, which adds
+# 1 to the value of each. Since we are using pass by
+# reference, the ACTUAL value of these integers is expected
+# to change, and this new value will persist outside of the
+# method where they are incremented.
+#
+# 25 + 23 = 48. Since 25 and 23 were both incremented by 1,
+# the new values should be 26 and 24. 
+# 26 + 24 = 50 which is what is the output generated for x + y.
+# x is now 26 which is the output for x.
